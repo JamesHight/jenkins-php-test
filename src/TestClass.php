f4c1091020a136032a7acfa414e95514f0e1e283
James Hight <james@zavoo.com>
2013-02-03 10:33:27 -0500

    Added class comments

--- src/TestClass.php
+++ src/TestClass.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+ * Simple class for PHPUnit test * 
+ **/
 class TestClass {
 
+	/**
+	 * Return Hello\n
+	 * @return String
+	 **/
 	public function hello() {
 		return "Hello\n";
 	}
