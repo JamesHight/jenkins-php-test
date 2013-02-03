51adb2d6dbeb4ce6094612632036e4d9a9975a52
James Hight <james@zavoo.com>
2013-02-03 11:00:24 -0500

    Added bad class

---
+++ src/derp.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Bad class
+ **/
+class Derp123 456 {
+	public private static const derp function 333() {
+
+	}
+}
--- tests/subdir/Test2Test.php
+++ tests/subdir/Test2Test.php
@@ -3,7 +3,7 @@ require_once 'PHPUnit/Autoload.php';
 
 class Test2Test extends PHPUnit_Framework_TestCase {
     function testHello() {
-        $this->assertEquals(1, 2);
+        $this->assertEquals(1, 1);
     }
 
 }
